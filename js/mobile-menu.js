8689a75cdcf33902953af00ac895f00fef9365bc
Andrii Berezhko <Berezhko7777@ukr.net>
2021-12-17 16:41:11 +0200

    ////
    осталось мобильное меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,14 +2,13 @@
     const menuBtnRef = document.querySelector('[data-menu-button]');
     const mobileMenuRef = document.querySelector("[data-menu]");
     menuBtnRef.addEventListener('Click', () => {
-        // const expanded =
-            menuBtnRef.getAttribute('aria-expanded') === "true" || false;
+        const expanded = menuBtnRef.getAttribute('aria-expanded') === "true" || false;
         
         menuBtnRef.classList.toggle('is-open');
         menuBtnRef.setAttribute('aria-expanded', !expanded);
 
         mobileMenuRef.classList.toggle('is-open');
-    })
+    });
 })();
 
 
