0e2a6d35dfea1672262adcec4559dd8ecb227d17
Andrii Berezhko <Berezhko7777@ukr.net>
2021-12-24 18:34:49 +0200

    /////

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -16,12 +16,14 @@
       openMenuBtn: document.querySelector('[data-menu-open]'),
       closeMenuBtn: document.querySelector('[data-menu-close]'),
       menu: document.querySelector('[data-menu]'),
+      // menu: document.querySelector('[no-scroll]'),
     };
   
     refs.openMenuBtn.addEventListener('click', toggleMenu);
     refs.closeMenuBtn.addEventListener('click', toggleMenu);
   
-    function toggleMenu() {
+  function toggleMenu() {
+      document.body.classList.toggle('menu-open')
       refs.menu.classList.toggle('is-hidden');
     }
   })();
